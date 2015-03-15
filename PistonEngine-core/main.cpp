74ace92057efa8555583674ca8dc9d8ab17b46a0
PistonMiner <pistonminer@gmail.com>
2015-03-15 18:36:56 +0100

    Added GLEW

---
+++ PistonEngine-core/main.cpp
@@ -0,0 +1,32 @@
+#include "src/graphics/window.h"
+
+int main()
+{
+	using namespace PistonEngine;
+	using namespace graphics;
+
+	Window window("PistonEngine", 960, 540);
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	while (!window.closed())
+	{
+		window.clear();
+#if 1
+		glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(-0.5f, 0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+#else
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
+		window.update();
+	}
+
+	return 0;
+}
---
+++ PistonEngine-core/src/graphics/window.cpp
@@ -0,0 +1,69 @@
+#include "window.h"
+
+namespace PistonEngine { namespace graphics {
+	
+	void windowResize(GLFWwindow *window, int width, int height);
+
+	Window::Window(const char *name, int width, int height)
+	{
+		m_Title = name;
+		m_Width = width;
+		m_Height = height;
+
+		if (!init())
+			glfwTerminate();
+	}
+
+	Window::~Window()
+	{
+		glfwTerminate();
+	}
+
+	bool Window::init()
+	{
+		if (!glfwInit())
+		{
+			std::cout << "Failed to initialize GLFW!" << std::endl;
+			return false;
+		}
+
+		m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+		if (!m_Window)
+		{
+			std::cout << "Failed to create GLFW window!" << std::endl;
+			return false;
+		}
+		glfwMakeContextCurrent(m_Window);
+		glfwSetWindowSizeCallback(m_Window, windowResize);
+		
+		if (glewInit() != GLEW_OK)
+		{
+			std::cout << "Failed to initialize GLEW!" << std::endl;
+			return false;
+		}
+		std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+		return true;
+	}
+
+	void Window::clear() const
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	void Window::update()
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(m_Window);
+	}
+
+	bool Window::closed() const
+	{
+		return glfwWindowShouldClose(m_Window) == 1;
+	}
+
+	void windowResize(GLFWwindow *window, int width, int height)
+	{
+		glViewport(0, 0, width, height);
+	}
+
+} }
